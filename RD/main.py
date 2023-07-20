a4a46adf46c73197bda79fe54a8b198c3cdecd77
imemmul <emrulurak17@outlook.com>
2023-07-20 14:57:57 +0300

    matching two images

--- RD/main.py
+++ RD/main.py
@@ -27,12 +27,42 @@ def load_engine():
         print(f"An error occured in load_engine: {e}")
 
 
+def find_match(img1, img2):
+    # Initialize the ORB detector algorithm
+    orb = cv2.ORB_create()
+
+    # Find keypoints and descriptors with ORB
+    kp1, des1 = orb.detectAndCompute(img1, None)
+    kp2, des2 = orb.detectAndCompute(img2, None)
+
+    # create BFMatcher object
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+    # Match descriptors.
+    matches = bf.match(des1, des2)
+
+    # Sort them in the order of their distance.
+    matches = sorted(matches, key = lambda x:x.distance)
+
+    # Draw only "good" matches (i.e., whose distance is less than 0.7*min_dist )
+    # and return keypoints in the second image that are matched
+    good = []
+    for m in matches:
+        good.append(m)
+    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+
+    # Draw matches
+    img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,flags=2)
+
+    return img3, dst_pts
+
+
 def run_inference(conf_thres, iou_thres):
     count = 0
     try:
         engine, device, H, W  = load_engine()
-        for image_dir in dataset_dir:
-            image = cv2.imread(filename=image_dir)
+        for image_dir in range(1):
+            image = cv2.imread(dataset_dir[0])
             try:
                 
                 bgr = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
@@ -55,45 +85,62 @@ def run_inference(conf_thres, iou_thres):
                     #print(f"nothing detected")
                 else:
                     print(f"what is bboxes type: {type(bboxes)}")
-                    random_idx = np.random.randint(len(bboxes))
-                    updated_bbox = torch.Tensor(list(bboxes[random_idx])).to(device=device)
-                    updated_scores = torch.Tensor([scores[random_idx]]).to(device=device)
-                    updated_masks =  masks[random_idx].to(device=device)
-                    updated_masks = updated_masks.unsqueeze(-1).expand(-1, -1, 3)
-                    updated_labels = torch.Tensor([labels[random_idx]]).to(device=device)
-                    updated_masks = updated_masks[:, dh:H - dh, dw:W - dw]
-                    indices = (updated_labels % len(MASK_COLORS)).long()
+                    random_index = np.random.randint(0, len(bboxes) - 1)
+                    masks = masks[random_index][None, dh:H - dh, dw:W - dw, :]
+                    indices = (labels[random_index] % len(MASK_COLORS)).long()
                     mask_colors = torch.asarray(MASK_COLORS, device=device)[indices]
                     mask_colors = mask_colors.view(-1, 1, 1, 3) * ALPHA
-                    mask_colors = updated_masks @ mask_colors
-                    inv_alph_masks = (1 - updated_masks * 0.5).cumprod(0)
-                    
+                    mask_colors = masks @ mask_colors
+                    inv_alph_masks = (1 - masks * 0.5).cumprod(0)
                     mcs = (mask_colors * inv_alph_masks).sum(0) * 2
-                    print(inv_alph_masks.shape)
-                    print(mcs.shape)
-                    print(seg_img.shape)
                     seg_img = (seg_img * inv_alph_masks[-1] + mcs) * 255
                     draw = cv2.resize(seg_img.cpu().numpy().astype(np.uint8),
                                     draw.shape[:2][::-1])
 
-                    updated_bbox -= dwdh
-                    updated_bbox /= ratio
+                    bboxes -= dwdh
+                    bboxes /= ratio
 
-                    for (bbox, score, label) in zip(updated_bbox, updated_scores, updated_labels):
-                        bbox = bbox.round().int().tolist()
-                        cls_id = int(label)
-                        cls = CLASSES[cls_id]
-                        color = COLORS[cls]
-                        cv2.rectangle(draw, bbox[:2], bbox[2:], color, 2)
-                        cv2.putText(draw,
-                                    f'{cls}:{score:.3f}', (bbox[0], bbox[1] - 2),
-                                    cv2.FONT_HERSHEY_SIMPLEX,
-                                    0.75, [225, 255, 255],
-                                    thickness=2)
+                    bbox = bboxes[random_index].round().int().tolist()
+                    score = scores[random_index]
+                    label = labels[random_index]
+                    cls_id = int(label)
+                    cls = CLASSES[cls_id]
+                    color = COLORS[cls]
+                    cv2.rectangle(draw, bbox[:2], bbox[2:], color, 2)
+                    cv2.putText(draw,
+                                f'{cls}:{score:.3f}', (bbox[0], bbox[1] - 2),
+                                cv2.FONT_HERSHEY_SIMPLEX,
+                                0.75, [225, 255, 255],
+                                thickness=2)
                     # print(f"image saved")
-                    count += 1
+                    img2 = cv2.imread(dataset_dir[1])
+                    margin = 200
+                    margine_bbox = bbox.copy()
+                    # Ensure bbox is within image boundaries
+                    margine_bbox[0] = max(0, bbox[0] - margin)  # x1
+                    margine_bbox[1] = max(0, bbox[1] - margin)  # y1
+                    margine_bbox[2] = min(image.shape[1], bbox[2] + margin)  # x2
+                    margine_bbox[3] = min(image.shape[0], bbox[3] + margin)  # y2
+                    
+                    # Cropping the detected hole from the original image
+                    cropped = image[margine_bbox[1]:margine_bbox[3], margine_bbox[0]:margine_bbox[2]]
+                    cv2.imwrite(f"./output/cropped_{count}.jpg", cropped)
                     #print(f"trying to save det here: {args.out_dir}run_{run_id}/{devices[cam_id]}/{exp_time}/DET/output_{capture_id}.jpg")
                     cv2.imwrite(filename=f"./output/output_{count}.jpg", img=draw)
+                    result, dst_pts = find_match(img1=cropped, img2=img2)
+                    x_coords = dst_pts[:,0,0]
+                    y_coords = dst_pts[:,0,1]
+                    bbox = [np.min(x_coords), np.min(y_coords), np.max(x_coords), np.max(y_coords)]
+
+                    # Draw the bounding box and segmentation mask in the second image
+                    # You might need to adjust the parameters to fit your case
+                    bbox = [int(coord) for coord in bbox]
+                    cv2.rectangle(img2, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0,255,0), 3)
+                    cv2.putText(img2, 'Hole', (bbox[0], bbox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+
+                    # Save the result
+                    cv2.imwrite('macthed_images.jpg', img2)
+                    count += 1
                 # print(f"Its been {end_time-start_time} seconds to process cam: {cam_id}")
             except Exception as e:
                 print(f"Some error occured in run_inference {e}")
